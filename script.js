ede22ee110b7b2b999b05e8db0e80da934244b51
NoorulAmeenJ <noorularafat786@gmail.com>
2022-12-27 17:11:13 +0530

    tried

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("mkkf")
+console.log("mkkf");
+
+function eight() {
+
+    // console.log("8")
+}
+var result = document.querySelector("#result");
+result.addEventListener("input", (e)=>{
+    console.log(e)
+}
+)
+var answer = document.querySelector("#answer");
+
+
+function equal() {
+    // console.log( eval(result.value))
+    var consol= eval(result.value)
+    console.log(consol)
+    answer.textContent = `${consol}`
+    // result.textContent = `: ${consol}`
+}
+
+result.style.backgroundColor = "pink";
+
+var three= document.getElementById('three');
