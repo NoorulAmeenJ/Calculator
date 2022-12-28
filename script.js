167a4252210a2401ab357c43f1991fadf9295fa5
NoorulAmeenJ <noorularafat786@gmail.com>
2022-12-28 09:38:17 +0530

    completed in html

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 
  var result =document.getElementById("result")
 function number(x){
-   
      result.value =   result.value + x
 }
 function Clear(){
-   
     result.value =  ""
 }
 function back(){
-   
     result.value = result.value.slice(0,-1);
-    
 }
 function equal(){
     result.value = eval(result.value)
